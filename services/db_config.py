1c06564df4c2341c7ca9be528b1ed81dfaafe084
Vince Roche <vince.roche@gmail.com>
2023-11-18 10:38:22 -0500

    config updates

--- services/db_config.py
+++ services/db_config.py
@@ -9,13 +9,6 @@ db_dialect = None
 def generate_uuid():
     return str(uuid.uuid4())
 
-
-# Conditionally set the UUID column type
-if db_dialect == 'postgresql':
-    UUIDType = UUID(as_uuid=True)
-elif db_dialect == 'mssql':
-    UUIDType = UNIQUEIDENTIFIER
-else:  # This will cover SQLite, MySQL, and other databases
-    UUIDType = String(36)
+ 
 
 
